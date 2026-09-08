0710720a172a133266bf33f44eef05fbec144b9e
阿部龍成 <s22005@tokyo.kosen-ac.jp>
2026-09-09 02:39:01 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
-})
+//% color="#2c3e50" icon="\uf06e" block="きょりセンサー"
+namespace distanceSensor {
+
+    //% block="まえに しょうがいぶつがある"
+    export function isObstacle(): boolean {
+        return pins.analogReadPin(AnalogReadWritePin.P2) >= 500;
+    }
+}
